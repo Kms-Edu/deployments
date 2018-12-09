0e9d83cd47488869353fb205f3088cd5fe359291
Dung Truong <checkraiser11@gmail.com>
2018-12-09 22:35:22 +0700

    Use ncc

--- www/env-config.js
+++ www/env-config.js
@@ -1,5 +1,5 @@
 //const prod = process.env.NODE_ENV === 'production'
 module.exports = {
-  'process.env.API_HOST': 'edu-1.herokuapp.com/v1alpha1/graphql',
+  'process.env.API_HOST': process.env.API_HOST,
   'process.env.NODE_ENV': process.env.NODE_ENV
 }
