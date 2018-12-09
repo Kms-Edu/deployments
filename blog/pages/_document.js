c108dd48645c342c3fca598e274768848aa12567
Dung Truong <checkraiser11@gmail.com>
2018-12-09 19:22:37 +0700

    Fix login

--- blog/pages/_document.js
+++ blog/pages/_document.js
@@ -11,11 +11,10 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <meta charset="utf-8" />
-          <meta http-equiv="x-ua-compatible" content="ie=edge" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
-          <style>{`body { margin: 0 } /* custom! */`}</style>
         </Head>
-        <body className="custom_class">
+        <body>
           <Main />
           <NextScript />
         </body>
